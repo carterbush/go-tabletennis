d8a4b1332880c9ff019084ec948f7a7f82809cbe
Carter Bush <carter.l.bush@gmail.com>
2021-02-10 00:28:20 +1100

    FE: Refresh the matches list once a second

--- frontend/src/hooks/MatchesProvider.tsx
+++ frontend/src/hooks/MatchesProvider.tsx
@@ -1,3 +1,4 @@
+import { waitFor } from '@testing-library/react'
 import React from 'react'
 import { GetAllMatches } from '../api/matchesAPI'
 import Match from '../types/match.interface'
@@ -6,9 +7,11 @@ const MatchesContext = React.createContext<Match[]>([])
 
 export const MatchesProvider: React.FunctionComponent<{}> = (props) => {
     const [matches, setMatches] = React.useState<Match[]>([])
+    const [timeup, setTimeup] = React.useState<boolean>(true)
     React.useEffect(() => {
         GetAllMatches().then(rs => setMatches(rs))
-    }, [])
+        setTimeout(() => setTimeup(!timeup), 1000)
+    }, [timeup])
 
     return (
         <MatchesContext.Provider value={matches}>
