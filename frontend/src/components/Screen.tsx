7c060e516ae7f0651720ee6e6689837bddebe9c8
Carter Bush <carter.l.bush@gmail.com>
2021-02-09 23:00:13 +1100

    FE: Add some progression to the selection screen

--- frontend/src/components/Screen.tsx
+++ frontend/src/components/Screen.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import ReporterList from './ReporterList'
 import MatchesList from './MatchesList'
+import ReportersContext from '../hooks/ReportersProvider'
 
 const Wrapper = styled.div`
     width: 100vw;
@@ -9,13 +10,39 @@ const Wrapper = styled.div`
     background-color: ${props => props.theme.colors.grayweb};
 `
 
+const Prompt = styled.h1`
+    margin: 0;
+    padding: 0;
+`
+
 const Screen = () => {
     const [selectedReporterId, setSelectedReporterId] = React.useState<string>('')
     const [selectedMatchId, setSelectedMatchId] = React.useState<string>('')
+    const reporters = React.useContext(ReportersContext)
+
+    const filteredReporters = reporters.filter(r => r.id === selectedReporterId)
+    const reporterName = filteredReporters.length > 0 ? filteredReporters[0].name : ''
+
+    let childComponent: JSX.Element
+    if (!selectedReporterId) {
+        childComponent =
+            <>
+                <Prompt>Please select yourself</Prompt>
+                <ReporterList selectedReporterId={selectedReporterId} onSelectReporterId={setSelectedReporterId} />
+            </>
+    } else if (!selectedMatchId) {
+        childComponent =
+            <>
+                <Prompt>Welcome, {reporterName}. Which match would you like to record?</Prompt>
+                <MatchesList selectedMatchId={selectedMatchId} onSelectMatchId={setSelectedMatchId}/>
+            </>
+    } else {
+        childComponent = <div>Derp</div>
+    }
+
     return (
         <Wrapper>
-          <ReporterList selectedReporterId={selectedReporterId} onSelectReporterId={setSelectedReporterId} />
-          <MatchesList selectedMatchId={selectedMatchId} onSelectMatchId={setSelectedMatchId}/>
+            {childComponent}
         </Wrapper>
     )
 }
