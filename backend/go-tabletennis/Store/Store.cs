846197f9ab38ef5c4b0f1f2a198b1d1151f416d8
Carter Bush <carter.l.bush@gmail.com>
2021-02-09 18:54:13 +1100

    Add some dummy data when creating the store

--- backend/go-tabletennis/Store/Store.cs
+++ backend/go-tabletennis/Store/Store.cs
@@ -21,7 +21,24 @@ namespace go_tabletennis
 
         public static Store Create()
         {
-            return new Store();
+            var store = new Store();
+
+            // Add some dummy data: players, matches and reporters
+            var jess = store.CreatePlayer("Jessica Jones");
+            var matt = store.CreatePlayer("Matthew Murdock");
+            var luke = store.CreatePlayer("Luke Cage");
+            var dan = store.CreatePlayer("Danny Rand");
+
+            store.CreateMatch(DateTime.Now.AddHours(1), jess, matt, 3);
+            store.CreateMatch(DateTime.Now.AddHours(2), jess, luke, 5);
+            store.CreateMatch(DateTime.Now.AddHours(3), matt, dan, 3);
+            store.CreateMatch(DateTime.Now.AddHours(4), dan, luke, 11);
+
+            store.CreateReporter("Mum");
+            store.CreateReporter("Stanley the Dog");
+            store.CreateReporter("Best Employee No 5");
+
+            return store;
         }
 
         public IEnumerable<Match> GetAllMatches()
@@ -38,5 +55,48 @@ namespace go_tabletennis
         {
             throw new NotImplementedException();
         }
+
+        private Guid CreateMatch(DateTime startTime, Guid p1, Guid p2, int numGames)
+        {
+            // Might fail
+            var player1 = this.Players[p1];
+            var player2 = this.Players[p2];
+
+            var id = Guid.NewGuid();
+            this.Matches[id] = new Match()
+            {
+                Id = id,
+                ScheduledStartTime = startTime,
+                Player1 = player1,
+                Player2 = player2,
+                NumGames = numGames,
+                Events = new List<MatchEvent>(),
+                ReporterId = null
+            };
+            return id;
+        }
+
+        private Guid CreatePlayer(string name)
+        {
+            var id = Guid.NewGuid();
+            this.Players[id] = new Player()
+            {
+                Id = id,
+                Name = name,
+            };
+            return id;
+        }
+
+        private Guid CreateReporter(string name)
+        {
+            var id = Guid.NewGuid();
+            this.Reporters[id] = new Reporter()
+            {
+                Id = id,
+                Name = name,
+            };
+            return id;
+        }
+
     }
 }
